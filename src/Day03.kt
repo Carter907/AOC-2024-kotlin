e46e2a2c4bfed01964b3545e894dd48cae72d04a
Carter <speers.carter@gmail.com>
2024-12-04 15:08:34 -0500

    completed day 4

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,6 +1,23 @@
 fun main() {
     fun part1(input: List<String>): Int {
+        val regex = Regex("mul\\(\\d{1,3},\\d{1,3}\\)")
+        var sum = 0
+        var match: MatchResult?
+        for (line in input) {
+            match = regex.find(line)
+            while (match != null) {
+                // add mul
+                val first = match.value.substringAfter("(").substringBefore(",").toInt()
+                val second = match.value.substringAfter(",").substringBefore(")").toInt()
+                sum += first * second
+                match = match.next()
+            }
 
+        }
+        return sum
+    }
+
+    fun part2(input: List<String>): Int {
         val regex = Regex("mul\\(\\d{1,3},\\d{1,3}\\)")
         val dodontregex = Regex("(don\'t\\(\\))|(do\\(\\))")
         var sum = 0
@@ -12,7 +29,7 @@ fun main() {
             match = regex.find(line, i)
             while (i < line.length && match != null) {
 
-                match.let {
+                match?.let {
                     dodontregex.findAll(line.substring(i, it.range.first)).let {
 
                         if (it.count() > 0 && it.last().value == "do()") {
@@ -26,24 +43,14 @@ fun main() {
                         val first = it.value.substringAfter("(").substringBefore(",").toInt()
                         val second = it.value.substringAfter(",").substringBefore(")").toInt()
                         sum += first * second
-
                     }
 
                     i = it.range.first + 1
                 }
-
-
                 match = regex.find(line, i)
             }
-
         }
-
-
         return sum
-    }
-
-    fun part2(input: List<String>): Any {
-        return 0
     }
 
 
---
+++ src/Day04.kt
@@ -0,0 +1,84 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        val mat = input.map { it.toCharArray() }
+        var count = 0
+
+        for (i in mat.indices) {
+            for (j in mat[i].indices) {
+                if (mat[i][j] == 'A') {
+
+                    // NorthEast
+                    if (j < mat[i].size - 3 && i > 2) {
+                        if (mat[i-1][j+1] == 'M' && mat[i-2][j+2] == 'A' && mat[i-3][j+3] == 'S') {
+                            count++
+                        }
+                    }
+                    // NorthWest
+                    if (j > 2 && i > 2) {
+                        if (mat[i-1][j-1] == 'M' && mat[i-2][j-2] == 'A' && mat[i-3][j-3] == 'S') {
+                            count++
+                        }
+                    }
+                    // SouthEast
+                    if (j < mat[i].size-3 && i < mat.size - 3) {
+                        if (mat[i+1][j+1] == 'M' && mat[i+2][j+2] == 'A' && mat[i+3][j+3] == 'S') {
+                            count++
+                        }
+                    }
+                    // SouthWest
+                    if (j > 2 && i < mat.size - 3) {
+                        if (mat[i+1][j-1] == 'M' && mat[i+2][j-2] == 'A' && mat[i+3][j-3] == 'S') {
+                            count++
+                        }
+                    }
+
+
+                }
+            }
+        }
+
+
+        return count
+    }
+
+    fun part2(input: List<String>): Int {
+        val mat = input.map { it.toCharArray() }
+        var count = 0
+
+        for (i in mat.indices) {
+            for (j in mat[i].indices) {
+                if (mat[i][j] == 'A') {
+                    if (i < mat.size - 1 && i > 0 && j > 0 && j < mat[i].size - 1) {
+                        // both forwards
+                        if (mat[i-1][j-1] == 'M' && mat[i+1][j+1] == 'S' && mat[i+1][j-1] == 'M' && mat[i-1][j+1] == 'S') {
+                            count++
+                        }
+                        // both backwards
+                        else if (mat[i-1][j-1] == 'S' && mat[i+1][j+1] == 'M' && mat[i+1][j-1] == 'S' && mat[i-1][j+1] == 'M') {
+                            count++
+                        }
+                        // first forward second backward
+                        else if (mat[i-1][j-1] == 'M' && mat[i+1][j+1] == 'S' && mat[i+1][j-1] == 'S' && mat[i-1][j+1] == 'M') {
+                            count++
+                        }
+                        // first backward second forward
+                        else if (mat[i-1][j-1] == 'S' && mat[i+1][j+1] == 'M' && mat[i+1][j-1] == 'M' && mat[i-1][j+1] == 'S') {
+                            count++
+                        }
+                    }
+                }
+            }
+        }
+
+
+        return count
+
+    }
+
+
+    // Read the input from the `src/Day01.txt` file.
+    val input = readInput("day4-input")
+    part1(input).println()
+    part2(input).println()
+
+}
